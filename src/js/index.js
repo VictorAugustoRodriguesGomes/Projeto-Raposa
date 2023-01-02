d098044d938e0c54160cfa6169ffd945fd4f6108
Victor Augusto <79682345+VictorAugustoRodriguesGomes@users.noreply.github.com>
2023-01-01 21:49:05 -0300

    base

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,41 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+let x = 0;
 
 const jump = () => {
-    mario.classList.add('jump');
+    mario.style.animation = "";
+    setTimeout(() => mario.style.animation = 'jump 500ms ease-out', 10);
+    console.log("j")
 }
 
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+
+        pipe.style.animation ='none';
+        pipe.style.left = `${pipePosition}px`;
+    
+        mario.style.animation ='none';
+        mario.style.left = `${marioPosition}px`;
+
+        mario.src = 'src/img/game/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+    }else{
+       
+    }
+
+    if(Keydown){
+        x = x + 1;
+        console.log(x);
+    }
+}, 10);
 
 document.addEventListener('keydown', jump);
